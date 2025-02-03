e93eb38bbf954c999f60d3c8b904e03aaf6ee0a9
A.M <hajiabdullrahmansilima@gmail.com>
2025-02-03 11:45:42 -0800

    The Fouth commit

--- drink/settings.py
+++ drink/settings.py
@@ -89,6 +89,13 @@ TEMPLATES = [
             ],
         },
     },
+    {
+        "BACKEND": "django.template.backends.jinja2.Jinja2",
+        "DIRS": [
+            "/home/html/jinja2",
+        ]
+    }
+    },
 ]
 
 # WSGI application
